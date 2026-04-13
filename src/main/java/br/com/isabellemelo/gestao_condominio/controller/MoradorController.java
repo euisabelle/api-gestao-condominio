fa6f1d820d88b0017ac788d742ac4f52a47600ba
Isabelle Melo <isabellefernandamelo@hotmail.com>
2026-04-12 23:05:53 -0300

    feat: adiciona rotas de atualizar e deletar morador

--- src/main/java/br/com/isabellemelo/gestao_condominio/controller/MoradorController.java
+++ src/main/java/br/com/isabellemelo/gestao_condominio/controller/MoradorController.java
@@ -22,4 +22,26 @@ public class MoradorController {
     public List<Morador> listar() {
         return repository.findAll();
     }
+
+    //metodo para atualizar um morador existente
+    @PutMapping("/{id}")
+    public Morador atualizar(@PathVariable Long id, @RequestBody Morador moradorAtualizado){
+        // Primeiro, o sistema vai no banco procurar se o morador existe
+        Morador moradorExistente = repository.findById(id).orElseThrow();
+
+        // Atualiza os dados com o que veio da internet
+        moradorExistente.setNome(moradorAtualizado.getNome());
+        moradorExistente.setApartamento(moradorAtualizado.getApartamento());
+        moradorExistente.setEmail(moradorAtualizado.getEmail());
+
+        // Salva as alterações no banco
+        return repository.save(moradorExistente);
+    }
+
+    // Método para Deletar um morador
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
 }
